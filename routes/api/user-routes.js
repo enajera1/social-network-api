a98766525af0e89060957fd9fb4a2b0334e4f856
enajera1 <eriknajera013@gmail.com>
2022-07-23 14:13:24 -0500

    user model/controller tune up to connect routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,2 +1,22 @@
 const router = require('express').Router();
+const { getAllUsers,
+getUserById,
+createUser,
+updateUser,
+deleteUser
+} = require('../../controllers/user-controller');
+
+//set up Get all and Post at /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+//set up Get one, put, and delete at /api/users/:id
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser)
+
 module.exports = router; 
